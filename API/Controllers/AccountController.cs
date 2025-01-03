1a1dc4fa76b9a1a5acacd385e6322e739b15178a
Ngô Việt Hoàng <162393499+Ngo-Viet-Hoang1@users.noreply.github.com>
2025-01-03 14:46:31 +0700

    feat(validation): add validation attributes to client data models

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -69,7 +69,7 @@ namespace API.Controllers
             user.Address = _mapper.Map<AddressDto, Address>(address);
 
             var result = await _userManager.UpdateAsync(user);
-
+ 
             if (result.Succeeded) return Ok(_mapper.Map<Address, AddressDto>(user.Address));
 
             return BadRequest("Problem updating the user");
@@ -97,6 +97,11 @@ namespace API.Controllers
         [HttpPost("register")]
         public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
         {
+            if (CheckEmailExistsAsync(registerDto.Email).Result.Value)
+            {
+                return new BadRequestObjectResult(new ApiValidationErrorResponse{Errors = new []{"Email address is in use"}});
+            }
+
             var user = new AppUser
             {
                 DisplayName = registerDto.DisplayName,
