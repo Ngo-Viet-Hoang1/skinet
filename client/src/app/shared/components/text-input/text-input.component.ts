24f4eaf0f6a3aca1b9342aa49c84aae060f2bee8
Ngô Việt Hoàng <162393499+Ngo-Viet-Hoang1@users.noreply.github.com>
2025-01-08 10:10:38 +0700

    fix(text-input): error not enough stack memory

--- client/src/app/shared/components/text-input/text-input.component.ts
+++ client/src/app/shared/components/text-input/text-input.component.ts
@@ -1,68 +1,31 @@
 import { CommonModule } from '@angular/common';
 import { Component, ElementRef, Input, OnInit, Self, ViewChild } from '@angular/core';
-import { ControlValueAccessor, NgControl } from '@angular/forms';
+import { ControlValueAccessor, FormControl, NgControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-text-input',
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './text-input.component.html',
   styleUrls: ['./text-input.component.scss']
 })
-export class TextInputComponent implements OnInit, ControlValueAccessor {
-  @ViewChild('input', { static: true }) input?: ElementRef;
+export class TextInputComponent implements ControlValueAccessor {
   @Input() type = 'text';
   @Input() label = '';
 
-  constructor(
-    @Self() public controlDir: NgControl
-  ) {
-    this.controlDir.valueAccessor = this
+  constructor(@Self() public controlDir: NgControl) {
+    this.controlDir.valueAccessor = this;
   }
-
-  ngOnInit() {
-    const control = this.controlDir.control;
-    if (control) {
-      const validators = control.validator ? [control.validator] : [];
-      const asyncValidators = control.asyncValidator ? [control.asyncValidator] : [];
-
-      control.setValidators(validators);
-      control.setAsyncValidators(asyncValidators);
-      control.updateValueAndValidity();
-    }
-  }
-
-  onChange(value: string) { }
-
-  onTouched() { }
 
   writeValue(obj: any): void {
-    if (this.controlDir.control) {
-      this.controlDir.control.setValue(obj, { emitEvent: false });
-    }
   }
-
   registerOnChange(fn: any): void {
-    this.onChange = fn;
   }
-
   registerOnTouched(fn: any): void {
-    this.onTouched = fn;
   }
 
-  setDisabledState?(isDisabled: boolean): void {
-    if (this.input) {
-      this.input.nativeElement.disabled = isDisabled;
-    }
+  get control(): FormControl {
+    return this.controlDir.control as FormControl
   }
 
-  handleInput(event: Event): void {
-    const value = (event.target as HTMLInputElement).value;
-    this.onChange(value);
-    this.controlDir.control?.setValue(value);
-  }
-
-  handleBlur(): void {
-    this.onTouched();
-  }
 
 }
